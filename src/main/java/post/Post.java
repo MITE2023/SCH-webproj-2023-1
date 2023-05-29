02b395703fb6d2d80c5a35df5bfe0b0a61954e9a
Damnun <shmom467@hanmail.net>
2023-05-29 22:34:29 +0900

    [ADD] Post, PostDAO (미완)
    post db 구조 설계 확인, user_no 넘기기/ 카테고리 분류 등

---
+++ src/main/java/post/Post.java
@@ -0,0 +1,89 @@
+package post;
+
+public class Post {
+    private int post_no;
+    private int img_no;
+    private int user_no;
+    private int comment_no;
+
+    public String getPost_date() {
+        return post_date;
+    }
+
+    public void setPost_date(String post_date) {
+        this.post_date = post_date;
+    }
+
+    private String post_date;
+
+
+    public int getPost_no() {
+        return post_no;
+    }
+
+    public void setPost_no(int post_no) {
+        this.post_no = post_no;
+    }
+
+    public int getImg_no() {
+        return img_no;
+    }
+
+    public void setImg_no(int img_no) {
+        this.img_no = img_no;
+    }
+
+    public int getUser_no() {
+        return user_no;
+    }
+
+    public void setUser_no(int user_no) {
+        this.user_no = user_no;
+    }
+
+    public int getComment_no() {
+        return comment_no;
+    }
+
+    public void setComment_no(int comment_no) {
+        this.comment_no = comment_no;
+    }
+
+    private String post_category;
+
+    public String getPost_category() {
+        return post_category;
+    }
+
+    public void setPost_category(String post_category) {
+        this.post_category = post_category;
+    }
+
+    public String getPost_title() {
+        return post_title;
+    }
+
+    public void setPost_title(String post_title) {
+        this.post_title = post_title;
+    }
+
+    public String getPost_code() {
+        return post_code;
+    }
+
+    public void setPost_code(String post_code) {
+        this.post_code = post_code;
+    }
+
+    public String getPost_context() {
+        return post_context;
+    }
+
+    public void setPost_context(String post_context) {
+        this.post_context = post_context;
+    }
+
+    private String post_title;
+    private String post_code;
+    private String post_context;
+}
---
+++ src/main/java/post/PostDAO.java
@@ -0,0 +1,108 @@
+package post;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PostDAO {
+    private Connection conn;
+    private ResultSet rs;
+
+    public PostDAO() {
+        try {
+            String dbURL = "jdbc:mysql://localhost:3306/test";
+            String dbID = "root";
+            String dbPassword = "root";
+            Class.forName("com.mysql.jdbc.Driver");
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getDate() {
+        String SQL = "SELECT NOW()";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ""; // DB error
+    }
+
+    public int getNext() {
+        String SQL = "SELECT post_no FROM post ORDER BY post_no DESC";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) + 1;
+            }
+            return 1;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int write(Post post, int userNo) { // TODO : user NO 어떻게?
+        String SQL = "INSERT INTO post (post_category, post_title, post_code, post_context, user_no, post_date) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, "1"); // TODO : 카테고리 분류
+            pstmt.setString(2, post.getPost_title());
+            pstmt.setString(3, post.getPost_code());
+            pstmt.setString(4, post.getPost_context());
+            pstmt.setInt(5, userNo);
+            pstmt.setString(6, getDate());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1; // DB error
+    }
+
+    public ArrayList<Post> getList(int pageNumber) {
+        String SQL = "SELECT * FROM post WHERE post_no < ? ORDER BY post_no DESC LIMIT 10";
+        ArrayList<Post> list = new ArrayList<Post>();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Post post = new Post();
+                post.setPost_no(rs.getInt(1));
+                post.setPost_category(rs.getString(2));
+                post.setPost_title(rs.getString(3));
+                post.setPost_code(rs.getString(4));
+                post.setPost_context(rs.getString(5));
+                post.setImg_no(rs.getInt(6));
+                post.setUser_no(rs.getInt(7));
+                post.setComment_no(8);
+                post.setPost_date(rs.getString(9));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public boolean nextPage(int pageNumber) {
+        String SQL = "SELECT * FROM post WHERE post_no < ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
